04e74ed8be2414a7037d73d81ae99380163a13a3
Da'Vonta Williamson <69822943+wdavonta@users.noreply.github.com>
2021-01-04 18:41:49 -0500

    added test function

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,1 +1,23 @@
 const Engineer = require('../lib/Engineer.js');
+const Employee = require('../lib/Employee');
+
+test ("can set github account to constructor argument", () => {
+    const testValue = "Githubname";
+    const employee = new Engineer ("Foo", 1, "test@test.com", testValue);
+    expect(employee.gitHub).toBe(testValue);
+})
+
+
+test ("Can get Githubname to getGithub", () => {
+    const testValue = "Githubname";
+    const employee = new Engineer ("Foo", 1, "test@test.com", "Githubname");
+    expect(employee.getGithub()).toBe(testValue)
+
+})
+
+
+test ("getRole() should return \"Engineer\"", ()=> {
+    const testValue = "Engineer";
+    const employee = new Engineer ("Foo", 1, "test@test.com", "Githubname");
+    expect(employee.getRole()).toBe(testValue);
+})
