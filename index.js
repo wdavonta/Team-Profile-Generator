83a73857466fd954cbacdfaa3460532b3a2e7116
Da'Vonta Williamson <69822943+wdavonta@users.noreply.github.com>
2021-01-04 21:18:06 -0500

    added const to index

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+const inquier = require ('inquirer');
+
+
+
+
 const Employee = require('.lib/Employee');
+const Manager = require ('./lib/Manager');
+const Engineer = require ('./lib/Engineer');
+const Intern = require ('.lib/Intern');
 
 new Employee().initializeEmployee();
